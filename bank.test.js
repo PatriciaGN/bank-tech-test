78ab9af5563b985b0fe23c7a7078d83afc2499f1
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-14 12:32:28 +0000

    Add failing test for .deposit()

--- bank.test.js
+++ bank.test.js
@@ -9,4 +9,9 @@ describe('Bank', () => {
   it('displays the default balance', () => {
     expect(bank.balance).toEqual(0);
   });
+
+  it('balance changes when user makes a deposit', () => {
+    bank.deposit(1000, '10-01-2023');
+    expect(bank.balance).toEqual(1000);
+  });
 });
