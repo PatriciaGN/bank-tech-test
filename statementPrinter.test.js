7c51a3888874280540678682f8e983b08da8c1a1
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 12:19:37 +0000

    Refactor to unit test by adding Bank mock

--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -1,14 +1,6 @@
 const StatementPrinter = require('./statementPrinter');
-const Bank = require('./bank.js');
 
 describe('StatementPrinter', () => {
-  let bank;
-  let statementPrinter;
-  beforeEach(() => {
-    bank = new Bank();
-    statementPrinter = new StatementPrinter(bank);
-  });
-
   it('prints a bank statement when no operations have been made', () => {
     expect(statementPrinter.printStatement()).toEqual(
       'date || credit || debit || balance'
@@ -16,8 +8,11 @@ describe('StatementPrinter', () => {
   });
 
   it('prints a bank statement when one deposit has been made', () => {
-    fakeDate = '10-12-2023';
-    bank.makeDeposit(1000.0);
+    const fakeBank = {
+      balance: 1000,
+      operations: [['deposit', 1000, '10-01-2023', 1000]],
+    };
+    statementPrinter = new StatementPrinter(fakeBank);
     expect(statementPrinter.printStatement()).toEqual(
       'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
     );
