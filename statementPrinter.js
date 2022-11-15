763764768f8a668d1a642fd05f5d7e65076910e0
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 13:04:00 +0000

    Refactor on printStatement()

--- statementPrinter.js
+++ statementPrinter.js
@@ -6,21 +6,23 @@ class StatementPrinter {
 
   printStatement() {
     let statement = 'date || credit || debit || balance';
-    function printOperations(operation) {
+
+    function creditOrDebit(operation) {
       if (operation[0] === 'deposit') {
-        statement += `\n${operation[2]} || ${operation[1].toFixed(
-          2
-        )} || || ${operation[3].toFixed(2)}`;
+        return `${operation[1].toFixed(2)} || `; // toFixed(2) converts the value into a two decimals float
       } else if (operation[0] === 'withdrawal') {
-        statement += `\n${operation[2]} || || ${operation[1].toFixed(
-          2
-        )} || ${operation[3].toFixed(2)}`;
+        return `|| ${operation[1].toFixed(2)} `;
       }
     }
     if (this.operations.length != 0) {
       this.operations
         .reverse()
-        .forEach((operation) => printOperations(operation));
+        .forEach(
+          (operation) =>
+            (statement += `\n${operation[2]} || ${creditOrDebit(
+              operation
+            )}|| ${operation[3].toFixed(2)}`)
+        );
     }
     return statement;
   }
