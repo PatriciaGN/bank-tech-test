a07ec159298746e77dd68b0d25fac87ac62d814b
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 12:35:08 +0000

    Remove extra new line after balance on first test, change date format to match requirements

--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -2,17 +2,24 @@ const StatementPrinter = require('./statementPrinter');
 
 describe('StatementPrinter', () => {
   it('prints a bank statement when no operations have been made', () => {
+    let fakeBank = {
+      balance: 0,
+      operations: [],
+    };
+    let statementPrinter = new StatementPrinter(fakeBank);
+
     expect(statementPrinter.printStatement()).toEqual(
-      'date || credit || debit || balance'
+      'date || credit || debit || balance\n'
     );
   });
 
   it('prints a bank statement when one deposit has been made', () => {
-    const fakeBank = {
+    let fakeBank = {
       balance: 1000,
-      operations: [['deposit', 1000, '10-01-2023', 1000]],
+      operations: [['deposit', 1000, '10/01/2023', 1000]],
     };
-    statementPrinter = new StatementPrinter(fakeBank);
+    let statementPrinter = new StatementPrinter(fakeBank);
+    console.log(statementPrinter.printStatement());
     expect(statementPrinter.printStatement()).toEqual(
       'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
     );
