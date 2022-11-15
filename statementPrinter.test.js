add9527639561824b0fa1482183efa76cee3bd6f
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 12:41:41 +0000

    Add failing test for printing a statement when two deposits have been made

--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -19,9 +19,22 @@ describe('StatementPrinter', () => {
       operations: [['deposit', 1000, '10/01/2023', 1000]],
     };
     let statementPrinter = new StatementPrinter(fakeBank);
-    console.log(statementPrinter.printStatement());
     expect(statementPrinter.printStatement()).toEqual(
       'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
+    );
+  });
+
+  it('prints a bank statement when two deposits has been made', () => {
+    let fakeBank = {
+      balance: 3000,
+      operations: [
+        ['deposit', 1000, '10/01/2023', 1000],
+        ['deposit', 2000, '13/01/2023', 3000],
+      ],
+    };
+    let statementPrinter = new StatementPrinter(fakeBank);
+    expect(statementPrinter.printStatement()).toEqual(
+      'date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00'
     );
   });
 });
