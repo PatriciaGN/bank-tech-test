0f32de1795fb850c87786f317a47ecb7a3a5a966
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 11:41:30 +0000

    Create DateStamp class to substitute manual date input

---
+++ dateStamp.js
@@ -0,0 +1,18 @@
+class DateStamp {
+  constructor() {
+    this.currentDate = new Date();
+  }
+
+  getDate() {
+    const currentDayOfMonth = this.currentDate.getDate();
+    const currentMonth = this.currentDate.getMonth(); // Be careful! January is 0, not 1
+    const currentYear = this.currentDate.getFullYear();
+
+    const dateString =
+      currentDayOfMonth + '-' + (currentMonth + 1) + '-' + currentYear;
+    // "27-11-2020"
+    return dateString;
+  }
+}
+
+module.exports = DateStamp;
