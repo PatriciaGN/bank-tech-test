bc795b8e294dbd6f196659e1d8d9f6cf51626802
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 11:43:25 +0000

    Refactor tests to work after introducing DateStamp

--- bank.test.js
+++ bank.test.js
@@ -11,90 +11,91 @@ describe('Bank', () => {
   });
 
   it('balance changes when user makes a deposit', () => {
-    bank.makeDeposit(1000, '10-01-2023');
+    bank.makeDeposit(1000);
+
     expect(bank.balance).toEqual(1000);
   });
 
   it('balance changes when more two deposits are made', () => {
-    bank.makeDeposit(1000, '10-01-2023');
-    bank.makeDeposit(500, '15-01-2023');
+    bank.makeDeposit(1000);
+    bank.makeDeposit(500);
+
     expect(bank.balance).toEqual(1500);
   });
 
   it('throws an error if amount is a negative number', () => {
     expect(() => {
-      bank.makeDeposit(-200, '10-01-2023');
+      bank.makeDeposit(-200);
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount equals zero', () => {
     expect(() => {
-      bank.makeDeposit(0, '10-01-2023');
+      bank.makeDeposit(0);
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount is a string', () => {
     expect(() => {
-      bank.makeDeposit('string', '10-01-2023');
+      bank.makeDeposit('string');
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount is a boolean', () => {
     expect(() => {
-      bank.makeDeposit(true, '10-01-2023');
+      bank.makeDeposit(true);
     }).toThrow('TypeError');
   });
 
   it('throws an error if a user tries to withdraw money and balance is zero', () => {
     expect(() => {
-      bank.makeWithdrawal(500, '15-01-2023');
+      bank.makeWithdrawal(500);
     }).toThrow('Insuficient funds');
   });
 
   it('throws an error if user tries to withdraw money and the amount is larger than the balance', () => {
-    bank.makeDeposit(200, '10-01-2023');
+    bank.makeDeposit(200);
     console.log(bank.balance);
-
     expect(() => {
-      bank.makeWithdrawal(600, '15-01-2023');
+      bank.makeWithdrawal(600);
     }).toThrow('Insuficient funds');
   });
 
   it('calculates the correct balance when user makes a deposit and withdraws funds', () => {
-    bank.makeDeposit(1000, '15-01-2023');
-    bank.makeWithdrawal(500, '15-01-2023');
+    bank.makeDeposit(1000);
+    bank.makeWithdrawal(500);
     expect(bank.balance).toEqual(500);
   });
 
   it('calculates the correct balance when user makes a deposit and withdraws funds', () => {
-    bank.makeDeposit(500, '15-01-2023');
-    bank.makeDeposit(200, '15-01-2023');
+    bank.makeDeposit(500);
+    bank.makeDeposit(200);
 
-    bank.makeWithdrawal(500, '15-01-2023');
+    bank.makeWithdrawal(500);
     expect(bank.balance).toEqual(200);
   });
 
   it('throws an error if amount is a negative number', () => {
     expect(() => {
-      bank.makeWithdrawal(-200, '10-01-2023');
+      bank.makeWithdrawal(-200);
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount equals zero', () => {
     expect(() => {
-      bank.makeWithdrawal(0, '10-01-2023');
+      bank.makeWithdrawal(0);
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount is a string', () => {
     expect(() => {
-      bank.makeWithdrawal('string', '10-01-2023');
+      bank.makeWithdrawal('string');
     }).toThrow('TypeError');
   });
 
   it('throws an error if amount is a boolean', () => {
     expect(() => {
-      bank.makeWithdrawal(true, '10-01-2023');
+      bank.makeWithdrawal(true);
     }).toThrow('TypeError');
   });
 
@@ -103,40 +104,26 @@ describe('Bank', () => {
   });
 
   it('stores a deposit into the operations array', () => {
-    bank.makeDeposit(500, '15-01-2023');
+    let date = '10-01-2023';
+    bank.makeDeposit(500, date);
 
-    expect(bank.operations).toContainEqual(['deposit', 500, '15-01-2023', 500]);
+    expect(bank.operations).toEqual([['deposit', 500, '10-01-2023', 500]]);
   });
 
   it('stores two deposits into the operations array', () => {
-    bank.makeDeposit(500, '15-01-2023');
-    bank.makeDeposit(600, '16-01-2023');
+    let date = '10-01-2023';
+    bank.makeDeposit(500, date);
+    bank.makeDeposit(600, date);
 
     expect(bank.operations.length).toBe(2);
-    expect(bank.operations).toContainEqual([
-      'deposit',
-      600,
-      '16-01-2023',
-      1100,
-    ]);
   });
 
   it('stores a withdrawal into the operations array', () => {
-    bank.makeDeposit(1000, '15-01-2023');
-    bank.makeWithdrawal(500, '16-01-2023');
+    let date = '10-01-23';
+
+    bank.makeDeposit(1000, date);
+    bank.makeWithdrawal(500, date);
 
     expect(bank.operations.length).toBe(2);
-    expect(bank.operations).toContainEqual([
-      'withdrawal',
-      500,
-      '16-01-2023',
-      500,
-    ]);
-  });
-
-  it('throws an error if date is incorrect format', () => {
-    expect(() => {
-      bank.makeDeposit(500, '10/01/2023');
-    }).toThrow('Incorrect date format');
   });
 });
