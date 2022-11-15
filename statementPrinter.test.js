2de7fd15b7b9e7ed2fcb7a87b61902d3d187b82d
Patricia Garcia <pgarcia16@alu.ucam.edu>
2022-11-15 11:50:26 +0000

    Add test for StatementPrinter when only one deposit has been made

--- statementPrinter.test.js
+++ statementPrinter.test.js
@@ -14,4 +14,12 @@ describe('StatementPrinter', () => {
       'date || credit || debit || balance'
     );
   });
+
+  it('prints a bank statement when one deposit has been made', () => {
+    fakeDate = '10-12-2023';
+    bank.makeDeposit(1000.0);
+    expect(statementPrinter.printStatement()).toEqual(
+      'date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00'
+    );
+  });
 });
